bbe92eb20c0fe4c85eab403a49fe57bd1b1ac3f8
yanbentes <yanwiverson@gmail.com>
2022-01-19 20:06:26 -0300

    att

--- init.el
+++ init.el
@@ -22,10 +22,6 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(custom-set-variables
- '(package-selected-packages (quote (eglot use-package smex evil dracula-theme company))))
-(custom-set-faces)
-
 ;; disable startup screen and backup files
 (setq inhibit-startup-screen t)
 (setq make-backup-files nil)
@@ -79,3 +75,17 @@
   (setq yas/triggers-in-field nil)
   (setq yas-snippet-dirs '("~/.emacs.d/yasnippets/"))
   (yas-global-mode 1))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yasnippet eglot use-package smex evil dracula-theme company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
