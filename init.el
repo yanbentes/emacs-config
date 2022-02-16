9f16b5d0a713f2963d2bf975ff2ffbca001371dc
Yan Bentes <yanwiverson@gmail.com>
2022-02-15 22:50:18 -0300

    more packages

--- init.el
+++ init.el
@@ -76,13 +76,20 @@
   (setq yas-snippet-dirs '("~/.emacs.d/yasnippets/"))
   (yas-global-mode 1))
 
+(use-package lua-mode
+  :ensure t)
+
+(use-package markdown-mode
+  :ensure t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yasnippet use-package smex evil dracula-theme)))
+   (quote
+    (markdown-mode lua-mode yasnippet use-package smex evil dracula-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
