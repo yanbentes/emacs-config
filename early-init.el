b593fdc766790ed91ce58b46ffacbd58d0a271cf
Yan Bentes <yanwiverson@gmail.com>
2022-02-17 21:41:55 -0300

    config rewrite without use-package

---
+++ early-init.el
@@ -0,0 +1,10 @@
+(defvar timu-package-list
+  '(yasnippet
+    python-mode
+    lua-mode
+    markdown-mode
+    smex
+    monokai-theme
+    dracula-theme)
+  "List of packages to be installed for the Emacs config to work as configured")
+
--- init.el
+++ init.el
@@ -1,4 +1,7 @@
-;; my github profile: https://github.com/yanbentes
+;; https://github.com/yanbentes
+(package-initialize)
+
+(load "~/.emacs.d/early-init.el")
 
 ;; display startup time
 (defun efs/display-startup-time ()
@@ -9,79 +12,63 @@
            gcs-done))
 (add-hook 'emacs-startup-hook #'efs/display-startup-time)
 
-;; initialize package sources
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+;; setup package installation
+(defun timu/packages-installed-p ()
+  "Check if all packages in `timu-package-list' are installed."
+  (cl-every #'package-installed-p timu-package-list))
 
-(require 'use-package)
-(setq use-package-always-ensure t)
+(defun timu/require-package (package)
+  "Install PACKAGE unless already installed."
+  (unless (memq package timu-package-list)
+    (add-to-list 'timu-package-list package))
+  (unless (package-installed-p package)
+    (package-install package)))
 
-;; disable startup screen and backup files
+(defun timu/require-packages (packages)
+  "Ensure PACKAGES are installed.
+Missing packages are installed automatically."
+  (mapc #'timu/require-package packages))
+
+(defun timu/install-packages ()
+  "Install all packages listed in `timu-package-list'."
+  (unless (timu/packages-installed-p)
+    ;; check for new packages (package versions)
+    (message "%s" "Reloading packages DB...")
+    (package-refresh-contents)
+    (message "%s" " done.")
+    ;; install the missing packages
+    (timu/require-packages timu-package-list)))
+
+;; run package installation
+(timu/install-packages)
+
 (setq inhibit-startup-screen t)
 (setq make-backup-files nil)
 
 (tool-bar-mode 0)
-(scroll-bar-mode 1)
+(scroll-bar-mode 0)
 (set-fringe-mode 5)
 (ido-mode 1)
 
-;; make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-;; display line numbers
-(column-number-mode)
-(global-display-line-numbers-mode t)
+(load-theme 'monokai t)
+;; (load-theme 'dracula t)
 
-;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-                shell-mode-hook
-                eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+(require 'smex)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
-;; dracula theme
-(use-package dracula-theme
-  :ensure t
-  :init
-  (load-theme 'dracula t))
-
-;; evil mode
-(use-package evil
-  :ensure t)
-
-;; org mode
-(use-package org
-  :ensure t
-  :init
-  (setq org-support-shift-select t))
-
-;; smex keybindings
-(use-package smex
-  :ensure t
-  :config							     	       
-  (global-set-key (kbd "M-x") 'smex)
-  (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-  (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command))
-
-;; yasnippet
-(use-package yasnippet
-  :ensure t
-  :config
-  (setq yas/triggers-in-field nil)
-  (setq yas-snippet-dirs '("~/.emacs.d/yasnippets/"))
-  (yas-global-mode 1))
-
-(use-package lua-mode
-  :ensure t)
-
-(use-package markdown-mode
-  :ensure t)
-
+(require 'yasnippet)
+(yas-global-mode 1)
+(setq yas/triggers-in-field nil)
+(setq yas-snippet-dirs '("~/.emacs.d/yasnippets/"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -89,7 +76,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (markdown-mode lua-mode yasnippet use-package smex evil dracula-theme))))
+    (dracula-theme monokai-theme smex markdown-mode lua-mode python-mode yasnippet))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
