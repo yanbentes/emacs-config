0b79a0f9f4fefb215eec5735ddd69f7e0faae201
Yan Bentes <yanwiverson@gmail.com>
2022-03-04 01:02:22 -0300

    how to use impatient-mode init.el comments

--- init.el
+++ init.el
@@ -74,6 +74,11 @@ Missing packages are installed automatically."
 (require 'auto-complete)
 (ac-config-default)
 
+;; to use impatient mode with markdown preview do
+;; M-x httpd-start
+;; M-x impatient-mode
+;; M-x imp-set-user-filter RET markdown-html RET
+(require 'impatient-mode)
 (defun markdown-html (buffer)
   (princ (with-current-buffer buffer
     (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
